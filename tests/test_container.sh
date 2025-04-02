0d24c6ffffff3ed65a2f469401ff787f9eb5dfe1
0FL01 <aluchinin@expertsolutions.ru>
2025-04-02 18:17:08 +0300

    change vars

--- tests/test_container.sh
+++ tests/test_container.sh
@@ -13,7 +13,7 @@ if [ -z "$TG_TOKEN" ]; then echo "Ошибка: TG_TOKEN не установле
 
 CONTAINER_NAME_TEST="avito-parser-test-${GITHUB_RUN_ID:-local}"
 ENV_FILE_TEST=".env.test"
-FOUND_AD_PATTERN_TEST="SUCCESS | __main__:__pretty_log"
+FOUND_AD_PATTERN_TEST="F6"
 TIMEOUT_SECONDS_TEST=120
 CHECK_INTERVAL_TEST=10
 
