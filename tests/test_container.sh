e3b4ddf69dbf96433f91c622a1eae72ffc28297e
0FL01 <aluchinin@expertsolutions.ru>
2025-04-02 18:20:34 +0300

    test time increased

--- tests/test_container.sh
+++ tests/test_container.sh
@@ -14,7 +14,7 @@ if [ -z "$TG_TOKEN" ]; then echo "Ошибка: TG_TOKEN не установле
 CONTAINER_NAME_TEST="avito-parser-test-${GITHUB_RUN_ID:-local}"
 ENV_FILE_TEST=".env.test"
 FOUND_AD_PATTERN_TEST="F6"
-TIMEOUT_SECONDS_TEST=120
+TIMEOUT_SECONDS_TEST=300
 CHECK_INTERVAL_TEST=10
 
 echo "Создание файла $ENV_FILE_TEST..."
